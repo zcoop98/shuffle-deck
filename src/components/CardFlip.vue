55e4ad07cafd9cf852f832f41dcc471de01024ab
Zach Leonard <zleonard@ambitenergy.com>
2020-05-14 15:33:16 -0500

    Initialize main page

---
+++ src/components/CardFlip.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-card
+        title="5 Card Poker"
+    >
+        <b-row>
+            <vue-playing-card height="200" cover />
+            <vue-playing-card height="200" cover />
+            <vue-playing-card height="200" cover />
+            <vue-playing-card height="200" cover />
+            <vue-playing-card height="200" cover />
+        </b-row>
+        <b-row>
+            <b-btn>
+                Re-Deal
+            </b-btn>
+        </b-row>
+    </b-card>
+</template>
+
+<script>
+import { VuePlayingCard } from 'vue-playing-card';
+
+export default {
+    components: {
+        VuePlayingCard,
+    },
+}
+</script>
